6665505784f5d1a5619a880b1d48053aa5c81649
Segni Kasahun <segnikasahun969@gmail.com>
2025-08-21 05:45:22 +0300

    updating root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,44 +1,8 @@
-// Import the global.css file in the index.js file:
 import '../global.css';
+import { Slot } from "expo-router";
 
-
-// app/_layout.tsx
-import { Slot, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../lib/firebase";
-import { View, ActivityIndicator } from "react-native";
 
 export default function RootLayout() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    const checkFlow = async () => {
-      const seen = await AsyncStorage.getItem("seenOnboarding");
-      onAuthStateChanged(auth, (user) => {
-        if (!seen) {
-          router.replace("/onboarding");
-        } else if (user) {
-          router.replace("/(tabs)/home");
-        } else {
-          router.replace("/(auth)/login");
-        }
-        setLoading(false);
-      });
-    };
-    checkFlow();
-  }, []);
-
-  if (loading) {
-    return (
-      <View className="flex-1 justify-center items-center bg-white mt-100">
-        <ActivityIndicator size="large" color="#2563eb" />
-      </View>
-    );
-  }
-
-  return <Slot />; // Render the correct route
+  return <Slot />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./app/**/*.{js,tsx,ts,jsx}", "./components/**/*.{js,jsx,ts,tsx}", "./hooks/**/*.{js,tsx,ts,jsx}", "./styles/**/*.{js,tsx,ts,jsx}",],
+  content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,tsx,ts,jsx}", "./components/**/*.{js,jsx,ts,tsx}", "./hooks/**/*.{js,tsx,ts,jsx}", "./styles/**/*.{js,tsx,ts,jsx}",],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
