746a08420cea2fbc1a261553cc61799855e050ec
Segni Kasahun <segnikasahun969@gmail.com>
2025-08-24 01:39:49 +0300

    updating screen option for modals

--- app/(modals)/_layout.tsx
+++ app/(modals)/_layout.tsx
@@ -7,6 +7,8 @@ export default function ModalLayout() {
         presentation: "modal",  // 👈 all screens in (modal) open as modals
         headerShown: false,     // or true if you want headers
         animation: "slide_from_bottom", // iOS style
+        gestureEnabled: true,   // enable swipe to close on iOS
+        gestureDirection: "vertical", // swipe down to close on iOS
       }}
     />
   );
