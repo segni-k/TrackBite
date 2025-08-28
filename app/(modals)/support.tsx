b09ba9a7f0580b0cf89dfb9d2560ee6f0fba6953
Segni Kasahun <segnikasahun969@gmail.com>
2025-08-28 23:10:14 +0300

    support screen

---
+++ app/(modals)/support.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Support = () => {
+  return (
+    <View>
+      <Text>support</Text>
+    </View>
+  )
+}
+export default Support
+
+const styles = StyleSheet.create({})
